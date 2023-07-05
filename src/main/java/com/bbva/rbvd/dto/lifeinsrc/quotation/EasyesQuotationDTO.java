d7f286ad491561a7f57cb3bbb57d5ab46b66db62
Aaron <carlos.carrillo.delgado@bbva.com>
2023-07-05 11:44:31 -0500

    feat: PERAR-23873 new fields term and refunds in EasyesQuotationDTO

--- src/main/java/com/bbva/rbvd/dto/lifeinsrc/quotation/EasyesQuotationDTO.java
+++ src/main/java/com/bbva/rbvd/dto/lifeinsrc/quotation/EasyesQuotationDTO.java
@@ -1,14 +1,17 @@
 package com.bbva.rbvd.dto.lifeinsrc.quotation;
 
-import com.bbva.rbvd.dto.lifeinsrc.commons.CommonFieldsDTO;
-import com.bbva.rbvd.dto.lifeinsrc.commons.HolderDTO;
-import com.bbva.rbvd.dto.lifeinsrc.commons.InsuranceProductDTO;
+import com.bbva.rbvd.dto.lifeinsrc.commons.*;
+
+import java.util.List;
 
 public class EasyesQuotationDTO extends CommonFieldsDTO {
 
     private InsuranceProductDTO product;
     private HolderDTO holder;
     private BankDTO bank;
+
+    private List<RefundsDTO> refunds;
+    private TermDTO term;
 
     public InsuranceProductDTO getProduct() {return product;}
     public void setProduct(InsuranceProductDTO product) {this.product = product;}
@@ -17,16 +20,31 @@ public class EasyesQuotationDTO extends CommonFieldsDTO {
     public BankDTO getBank() {return bank;}
     public void setBank(BankDTO bank) {this.bank = bank;}
 
+    public List<RefundsDTO> getRefunds() {
+        return refunds;
+    }
+
+    public void setRefunds(List<RefundsDTO> refunds) {
+        this.refunds = refunds;
+    }
+
+    public TermDTO getTerm() {
+        return term;
+    }
+
+    public void setTerm(TermDTO term) {
+        this.term = term;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("EasyesQuotationDTO{");
         sb.append("product=").append(product);
         sb.append(", holder=").append(holder);
-        sb.append(", isDataTreatment='").append(this.getIsDataTreatment()).append('\'');
-        sb.append(", externalSimulationId='").append(this.getExternalSimulationId()).append('\'');
         sb.append(", bank=").append(bank);
+        sb.append(", refunds=").append(refunds);
+        sb.append(", term=").append(term);
         sb.append('}');
         return sb.toString();
     }
-
 }
