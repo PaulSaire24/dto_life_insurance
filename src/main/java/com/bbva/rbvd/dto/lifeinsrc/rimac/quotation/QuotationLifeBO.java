15850c08c77d036052f8c41315613aa03e7e85d6
Fredy Cerron <fredy.cerron.contractor@bbva.com>
2023-08-14 11:43:31 -0500

    feat:PERAR-24286 PERAR-24519 add and change QuotationLifeDTO, QuotationLifePayloadBO, QuotationLifeBO and MockData

---
+++ src/main/java/com/bbva/rbvd/dto/lifeinsrc/rimac/quotation/QuotationLifeBO.java
@@ -0,0 +1,8 @@
+package com.bbva.rbvd.dto.lifeinsrc.rimac.quotation;
+
+public class QuotationLifeBO {
+    private EasyesQuotationPayloadBO payload;
+
+    public EasyesQuotationPayloadBO getPayload() {return payload;}
+    public void setPayload(EasyesQuotationPayloadBO payload) {this.payload = payload;}
+}
