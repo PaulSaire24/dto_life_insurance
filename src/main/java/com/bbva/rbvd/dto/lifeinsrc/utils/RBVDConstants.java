d51dee98d1dd18437ad8736d74326650357acd85
paulkyev <paul.saire.contractor@bbva.com>
2023-09-29 14:05:50 -0500

    perf: added contructor private RBVDConstan

--- src/main/java/com/bbva/rbvd/dto/lifeinsrc/utils/RBVDConstants.java
+++ src/main/java/com/bbva/rbvd/dto/lifeinsrc/utils/RBVDConstants.java
@@ -1,8 +1,14 @@
 package com.bbva.rbvd.dto.lifeinsrc.utils;
 
 public class RBVDConstants {
+    private RBVDConstants() {
+    }
+
     public static final String AUTHORIZATION = "Authorization";
     public static final class Customer{
+        private Customer() {
+        }
+
         public static final String MALE = "MALE";
         public static final String FEMALE = "FEMALE";
         public static final String EMAIL = "EMAIL";
@@ -10,6 +16,8 @@ public class RBVDConstants {
         public static final String PHONE_NUMBER ="PHONE_NUMBER";
     }
     public static final class Crypto{
+        private Crypto() {
+        }
         public static final String APP_NAME ="APP_NAME";
         public static final String OAUTH_TOKEN ="";
         public static final String CRE_EXTRA_PARAMS ="CRE_EXTRA_PARAMS";
