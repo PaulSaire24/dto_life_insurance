2192bc29b29a68e4d8c00e9669fde2846dcba0d9
paulkyev <paul.saire.contractor@bbva.com>
2023-09-29 13:51:03 -0500

    perf: PERAR-25185 added sub-class RBVDConstan

--- src/main/java/com/bbva/rbvd/dto/lifeinsrc/utils/RBVDConstants.java
+++ src/main/java/com/bbva/rbvd/dto/lifeinsrc/utils/RBVDConstants.java
@@ -1,15 +1,24 @@
 package com.bbva.rbvd.dto.lifeinsrc.utils;
 
 public class RBVDConstants {
-    public static final String APP_NAME ="APP_NAME";
-    public static final String OAUTH_TOKEN ="";
-    public static final String CRE_EXTRA_PARAMS ="CRE_EXTRA_PARAMS";
-    public static final String INPUT_TEXT_SECURITY ="INPUT_TEXT_SECURITY";
-    public static final String B64URL = "B64URL";
     public static final String AUTHORIZATION = "Authorization";
-    public static final String MALE = "MALE";
-    public static final String FEMALE = "FEMALE";
-    public static final String EMAIL = "EMAIL";
-    public static final String MOBILE_NUMBER = "MOBILE_NUMBER";
-    public static final String PHONE_NUMBER ="PHONE_NUMBER";
+    public static final class Customer{
+        public static final String MALE = "MALE";
+        public static final String FEMALE = "FEMALE";
+        public static final String EMAIL = "EMAIL";
+        public static final String MOBILE_NUMBER = "MOBILE_NUMBER";
+        public static final String PHONE_NUMBER ="PHONE_NUMBER";
+    }
+    public static final class Crypto{
+        public static final String APP_NAME ="APP_NAME";
+        public static final String OAUTH_TOKEN ="";
+        public static final String CRE_EXTRA_PARAMS ="CRE_EXTRA_PARAMS";
+        public static final String INPUT_TEXT_SECURITY ="INPUT_TEXT_SECURITY";
+        public static final String B64URL = "B64URL";
+    }
+
+
+
+
+
 }
